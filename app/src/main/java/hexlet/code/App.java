6ffef2832ac27209f7311fc3d38d55727d7c7d67
Andrey Matis <androidmatis@gmail.com>
2024-03-13 18:52:16 +0600

    Update CLI (create Parameters and Option) st.4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,9 +2,19 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+import java.io.File;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private File filepath1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private File filepath2;
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish", description = "output format [default: stylish]")
+    private String format;
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
